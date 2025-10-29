238bbfdaf52f6385e62452b67b0a86fc154f108f
oseiasdfarias <oseias.dfarias@gmail.com>
2025-10-29 01:45:01 -0300

    deploy: deploy project to vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -172,7 +172,7 @@ function App() {
       setInput('');
 
       try {
-          const response = await fetch('http://localhost:8000/chat', { // URL para dev local
+          const response = await fetch('/api/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ session_id: sessionId, message: userMessageContent }),
